8dd1293cf73961134c80a58b93acf35e7ed4a0d1
ketanbotz07 <ketankumar8567@gmail.com>
2025-12-15 02:37:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
+import os
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
-from bot.config import BOT_TOKEN
-from bot.handlers import start, handle_video
+from handlers import start, handle_video
 
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+if not BOT_TOKEN:
+    raise RuntimeError("BOT_TOKEN environment variable not set")
 
 app = ApplicationBuilder().token(BOT_TOKEN).build()
-
 
 app.add_handler(CommandHandler("start", start))
 app.add_handler(MessageHandler(filters.VIDEO, handle_video))
 
-
+print("🤖 Bot started successfully...")
 app.run_polling()
