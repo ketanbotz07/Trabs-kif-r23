a942658432bfed4cbe7dd23e079fe7e8599d2b03
ketanbotz07 <ketankumar8567@gmail.com>
2026-01-02 23:39:51 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+
+SEGMENT_TIME = 2 # seconds
+MAX_DURATION = 900 # 15 minutes
+TEMP_DIR = "temp"
